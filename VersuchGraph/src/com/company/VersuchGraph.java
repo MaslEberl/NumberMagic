64ec78c4f265a79de2a09af130ac1cba367e43ec
Birgit Baumann <bb.baumann@aon.at>
2017-05-12 09:35:52 +0200

    2017-05-12 Überarbeitung
    Geänderte Version in JavaFX

--- VersuchGraph/src/com/company/VersuchGraph.java
+++ VersuchGraph/src/com/company/VersuchGraph.java
@@ -1,83 +1,50 @@
 package com.company;
-import java.awt.*;
-import java.awt.font.*;
-import java.awt.geom.*;
-import javax.swing.*;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
 
-public class VersuchGraph extends JPanel {
-    int[] data = {
-            21, 14, 18, 03, 86, 88, 74, 87, 54, 77,
-            61, 55, 48, 60, 49, 36, 38, 27, 20, 18
-    };
-    final int PAD = 20;
 
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D)g;
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-                RenderingHints.VALUE_ANTIALIAS_ON);
-        int w = getWidth();
-        int h = getHeight();
-        // Draw ordinate.
-        g2.draw(new Line2D.Double(PAD, PAD, PAD, h-PAD));
-        // Draw abcissa.
-        g2.draw(new Line2D.Double(PAD, h-PAD, w-PAD, h-PAD));
-        // Draw labels.
-        Font font = g2.getFont();
-        FontRenderContext frc = g2.getFontRenderContext();
-        LineMetrics lm = font.getLineMetrics("0", frc);
-        float sh = lm.getAscent() + lm.getDescent();
-        // Ordinate label.
-        String s = "data";
-        float sy = PAD + ((h - 2*PAD) - s.length()*sh)/2 + lm.getAscent();
-        for(int i = 0; i < s.length(); i++) {
-            String letter = String.valueOf(s.charAt(i));
-            float sw = (float)font.getStringBounds(letter, frc).getWidth();
-            float sx = (PAD - sw)/2;
-            g2.drawString(letter, sx, sy);
-            sy += sh;
-        }
-        // Abcissa label.
-        s = "x axis";
-        sy = h - PAD + (PAD - sh)/2 + lm.getAscent();
-        float sw = (float)font.getStringBounds(s, frc).getWidth();
-        float sx = (w - sw)/2;
-        g2.drawString(s, sx, sy);
-        // Draw lines.
-        double xInc = (double)(w - 2*PAD)/(data.length-1);
-        double scale = (double)(h - 2*PAD)/getMax();
-        g2.setPaint(Color.green.darker());
-        for(int i = 0; i < data.length-1; i++) {
-            double x1 = PAD + i*xInc;
-            double y1 = h - PAD - scale*data[i];
-            double x2 = PAD + (i+1)*xInc;
-            double y2 = h - PAD - scale*data[i+1];
-            g2.draw(new Line2D.Double(x1, y1, x2, y2));
-        }
-        // Mark data points.
-        g2.setPaint(Color.red);
-        for(int i = 0; i < data.length; i++) {
-            double x = PAD + i*xInc;
-            double y = h - PAD - scale*data[i];
-            g2.fill(new Ellipse2D.Double(x-2, y-2, 4, 4));
-        }
-    }
+public class VersuchGraph extends Application {
 
-    private int getMax() {
-        int max = -Integer.MAX_VALUE;
-        for(int i = 0; i < data.length; i++) {
-            if(data[i] > max)
-                max = data[i];
-        }
-        return max;
+    @Override public void start(Stage stage) {
+        stage.setTitle("Line Chart Sample");
+        //defining the axes
+        final NumberAxis xAxis = new NumberAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        xAxis.setLabel("Number of Month");
+        //creating the chart
+        final LineChart<Number,Number> lineChart =
+                new LineChart<Number,Number>(xAxis,yAxis);
+
+        lineChart.setTitle("Stock Monitoring, 2010");
+        //defining a series
+        XYChart.Series series = new XYChart.Series();
+        series.setName("My portfolio");
+        //populating the series with data
+        series.getData().add(new XYChart.Data(1, 23));
+        series.getData().add(new XYChart.Data(2, 14));
+        series.getData().add(new XYChart.Data(3, 15));
+        series.getData().add(new XYChart.Data(4, 24));
+        series.getData().add(new XYChart.Data(5, 34));
+        series.getData().add(new XYChart.Data(6, 36));
+        series.getData().add(new XYChart.Data(7, 22));
+        series.getData().add(new XYChart.Data(8, 45));
+        series.getData().add(new XYChart.Data(9, 43));
+        series.getData().add(new XYChart.Data(10, 17));
+        series.getData().add(new XYChart.Data(11, 29));
+        series.getData().add(new XYChart.Data(12, 25));
+
+        Scene scene  = new Scene(lineChart,800,600);
+        lineChart.getData().add(series);
+
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
-        JFrame f = new JFrame();
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.add(new VersuchGraph());
-        f.setSize(400,400);
-        f.setLocation(200,200);
-        f.setVisible(true);
+        launch(args);
     }
 }
