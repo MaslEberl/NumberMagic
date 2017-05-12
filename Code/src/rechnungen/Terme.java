0bbb96beddc9dc68a770263ca97a161436c3792a
Julian Wollersberger <julian.derzocker@gmx.at>
2017-05-12 09:21:46 +0200

    2017-05-12 Mit der Klasse Terme angefangen

--- Code/src/rechnungen/Terme.java
+++ Code/src/rechnungen/Terme.java
@@ -2,7 +2,15 @@ package rechnungen;
 
 import java.util.ArrayList;
 
+/** In dieser Klasse werden die Zahlen mit Operationen gespeichert.<br>
+ * Es gibt eine Member-Variable für den ersten Wert. Alle anderen
+ * sind als Term (Paar von einer Operation mit dem darauf folgenden Wert)
+ * in einer ArrayList gespeichert.<br>
+ *
+ * Weil diese Klasse von ArrayList erbt, können alle Methoden einer
+ * ArrayList verwendet werden, z.B. terme.add(term1) */
 public class Terme extends ArrayList<Term>
 {
+    double startwert;
 
 }
