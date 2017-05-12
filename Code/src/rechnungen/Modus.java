6261b59498e567ef67311b83c7d6c6ec6bab56a5
Lukas09091999 <luigi.puehringer@gmail.com>
2017-05-12 09:05:40 +0200

    2017-05-12 Erstellen der Modus-Klasse mit Beginnen

---
+++ Code/src/rechnungen/Modus.java
@@ -0,0 +1,12 @@
+
+
+public class Modus{
+   private int minZahl1;
+   private int minZahl2;
+   private int maxZahl1;
+   private int maxZahl2;
+   private int kommaZahl1;
+   private int kommaZahl2;
+
+
+}
