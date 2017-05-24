208851247f90e65f050df0d898a869e04c1f1913
Lukas09091999 <luigi.puehringer@gmail.com>
2017-05-24 13:04:58 +0200

    2017-05-23 Vorläufiges Fertigstellen der Klasse Modus und Grenzen

--- Code/src/rechnungen/Modus.java
+++ Code/src/rechnungen/Modus.java
@@ -6,8 +6,11 @@ import java.util.TreeMap;
 public class Modus{
       TreeMap<Operation,Grenzen> map = new TreeMap<>();
 
+      //Liest aus der Map herraus und fixiert Grenzen
+
       Grenzen getForOperationen(Operation op){
-            
+            //Gibt die Grenzen, für den Key in der Map gespeichert sind zurück
+          return map.get(op);
       }
 
 }
