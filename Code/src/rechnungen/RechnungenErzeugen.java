00f87fa70f225ab4c32f047f21d9b63f73ba4ab8
Julian Wollersberger <julian.derzocker@gmx.at>
2017-05-31 08:41:27 +0200

    2017-05-31 RechnungenErzeugen Klasse erstellt. Ich habe hineingeschrieben, wie ich mir vorstelle, wie es funktionieren soll.

---
+++ Code/src/rechnungen/RechnungenErzeugen.java
@@ -0,0 +1,23 @@
+package rechnungen;
+
+/**
+ * Created by julian on 31.05.17.
+ */
+public class RechnungenErzeugen {
+
+    /*
+    * Wie es funktionieren soll:
+    *
+    * Es wird unterteilt in einzelne Terme erzeugen.
+    *
+    * erzeugeTerm() wählt zuerst eine Operation und versucht, Zufallszahlen zu wählen,
+    * die die übergebenen Grenzen für diese Operation einhalten. Nach ein paar
+    * Versuchen soll es fehlschlagen.
+    * Das soll so sein, damit man beim Erzeugen nicht in eine Sackgasse gerät.
+    *
+    * Wenn das erzeugen eines Terms fehlschlägt, sollen ein paar Terme davor
+    * auch gelöscht werden. Rekursiv?, Jedes mal 5 Versuche?
+    *
+    *
+    * */
+}
